3b403ec1da8aa1cb0057b65655c77a3ca2d7710d
MaxymBeregovoi <108676512+maksberegovoi@users.noreply.github.com>
2026-02-14 23:49:19 +0200

    fix redux persist

--- src/redux/config/persistConfig.js
+++ src/redux/config/persistConfig.js
@@ -4,7 +4,7 @@ import cartReducer from '../features/cart/slice/cartSlice.js'
 
 export const persistedCartReducer = persistReducer(
     {
-        key: 'cart',
+        key: 'cart_v2',
         version: 2,
         storage,
         whitelist: ['items']
