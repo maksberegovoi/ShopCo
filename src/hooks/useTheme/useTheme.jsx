dda0b3c2624a809ce10f33b3ba7509b0125071d7
MaxymBeregovoi <108676512+maksberegovoi@users.noreply.github.com>
2026-02-14 22:59:48 +0200

    fix CI build

--- src/hooks/useTheme/useTheme.jsx
+++ src/hooks/useTheme/useTheme.jsx
@@ -1,5 +1,5 @@
 import { useContext } from 'react'
-import { ThemeContext } from './ThemeProvider.jsx'
+import { ThemeContext } from './ThemeContext.js'
 
 export const useTheme = () => {
     const ctx = useContext(ThemeContext)
